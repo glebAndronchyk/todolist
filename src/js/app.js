37bb3c3752c7505e530e6ccb4bec30b118886846
Gleb <gleb.andronchyk@gmail.com>
2022-10-23 16:51:07 +0300

    application-skeleton

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,105 @@
 import '../scss/app.scss';
+import { setDataToLocalStorage, initLocalStorage, removeItemFromLocalStorage, changeDoneStatus, getAllData } from './localStorage';
+import { v4 as uuidv4 } from 'uuid';
 
 /* Your JS Code goes here */
+document.addEventListener('DOMContentLoaded', () => {
+    const DATA = initLocalStorage();
+    renderList(DATA);
+    const form = document.querySelector('#form');
+    const filterButtons = document.querySelectorAll('.filter-btn');
 
-/* Demo JS */
-import './demo.js';
+    filterButtons.forEach((btn) => {
+        btn.addEventListener('click', priorityFilter);
+    });
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const eventTarget = event.target;
+        const formData = {
+            taskName: eventTarget.elements["input-text"].value,
+            priority: eventTarget.elements["priority-select"].value,
+            isDone: false,
+            id: uuidv4(),
+        };
+        if (!formData.taskName) {
+            return;
+        }
+        setDataToLocalStorage(formData);
+        renderListItem(formData);
+        eventTarget.reset();
+    });
+
+    function renderList(arr) {
+        arr.forEach(item => {
+            renderListItem(item);
+        });
+    }
+
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    function priorityFilter(event) {
+        const btnValue = event.target.value;
+        const data = getAllData();
+        const tasksList = document.querySelector('#tasks-list');
+        removeAllChildNodes(tasksList);
+        if (btnValue === 'all') {
+            renderList(data);
+            return;
+        }
+        const filteredData = data.filter((item) => item.priority === btnValue);
+        renderList(filteredData);
+    }
+
+    function isCompletedChecker(event) {
+        const parent = event.target.parentElement;
+        const ID = parent.getAttribute('data-id');
+        const CHECKED = event.target.checked;
+        changeDoneStatus(ID, CHECKED);
+        if (CHECKED) {
+            parent.classList.add('done');
+        } else {
+            parent.classList.remove('done');
+        }
+    }
+
+    function deleteItem(event) {
+        const parent = event.target.parentElement;
+        const ID = parent.getAttribute('data-id');
+        removeItemFromLocalStorage(ID);
+        parent.remove();
+    }
+
+    function renderListItem(listItem) {
+        const tasksList = document.querySelector('#tasks-list');
+        const taskItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        const isCompletedCheckbox = document.createElement('input');
+
+        isCompletedCheckbox.setAttribute('type', 'checkbox');
+        isCompletedCheckbox.setAttribute('name', 'isCompleted');
+        isCompletedCheckbox.checked = listItem.isDone;
+
+        taskItem.innerHTML = `<span>${listItem.taskName}, priority: ${listItem.priority}</span>`;
+        taskItem.classList.add('list-item');
+        taskItem.setAttribute('data-id', listItem.id);
+
+        deleteButton.innerHTML = 'X';
+        deleteButton.classList.add('delete-button');
+
+        if (listItem.isDone) {
+            taskItem.classList.add('done');
+        }
+
+        taskItem.prepend(isCompletedCheckbox);
+        taskItem.append(deleteButton);
+        tasksList.append(taskItem);
+
+        deleteButton.addEventListener('click', deleteItem);
+        isCompletedCheckbox.addEventListener('change', isCompletedChecker);
+    } 
+});
---
+++ src/js/localStorage.js
@@ -0,0 +1,33 @@
+export const setDataToLocalStorage = (tasks) => {
+    const items = JSON.parse(localStorage.getItem('TODOLIST'));
+    const newData = [...items, tasks];
+    localStorage.setItem('TODOLIST', JSON.stringify(newData));
+};
+export const initLocalStorage = () => {
+    if (localStorage.getItem('TODOLIST')) {
+        return JSON.parse(localStorage.getItem('TODOLIST'));
+    }
+    localStorage.setItem('TODOLIST', JSON.stringify([]));
+    return [];
+};
+
+export const removeItemFromLocalStorage = (id) => {
+    const itemsData = JSON.parse(localStorage.getItem('TODOLIST'));
+    const newData = itemsData.filter((item) => item.id != id);
+    localStorage.setItem('TODOLIST', JSON.stringify(newData));
+};
+
+export const changeDoneStatus = (id, value) => {
+    const itemsData = JSON.parse(localStorage.getItem('TODOLIST'));
+    const newData = itemsData.map((item) => {
+       if (id == item.id) {
+        return {...item, isDone: value};
+       } 
+       return item;
+    });
+    localStorage.setItem('TODOLIST', JSON.stringify(newData));
+};
+
+export const getAllData = () => {
+    return JSON.parse(localStorage.getItem('TODOLIST'));
+};
